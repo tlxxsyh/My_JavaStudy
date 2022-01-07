fc5d3771ced4338e018fa16875c0f7aaf5f61562
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2022-01-07 17:42:10 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
+//我的第一个java程序
+//java程序必须从main()程序开始
+//main()程序必须按照public static void main()这样写
+
 public class first {
     public static void main(String[] args) {
         System.out.println("Hello World!");
