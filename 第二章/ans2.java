12f551ab8366f1e88205fe495b330bc3135a6a22
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2022-01-07 18:02:23 +0800

    Create ans2.java

---
+++ 第二章/ans2.java
@@ -0,0 +1,9 @@
+public class ans2 {
+    public static void main(String[] args) {
+       int a=2,b=3;
+       System.out.println(a+b);
+       System.out.println(a-b);
+       System.out.println(a*b);
+       System.out.println(a/b);
+    }
+}
