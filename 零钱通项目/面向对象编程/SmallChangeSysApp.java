ea41cc65fec318693e560732ab1455f7f38614d7
言念君君子 <61855391+tlxxsyh@users.noreply.github.com>
2022-01-29 23:00:14 +0800

    Create SmallChangeSysApp.java

---
+++ 零钱通项目/面向对象编程/SmallChangeSysApp.java
@@ -0,0 +1,10 @@
+package pers.shangyuhang.smallchange;
+
+/**
+ * 本类是零钱通的主类，调用零钱通类来完成程序
+ */
+public class SmallChangeSysApp {
+    public static void main(String[] args) {
+        new SmallChangeSysOOP().mainMenu();
+    }
+}
